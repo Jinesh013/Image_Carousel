e3eaec7e9fc151c64203c70ad99e9fbe6e592536
Jinesh Parekh <109714163+Jinesh013@users.noreply.github.com>
2024-06-26 23:05:31 +0530

    Add files via upload

---
+++ Assignment 5/script1.js
@@ -0,0 +1,13 @@
+let myColor = ["Red", "Green", "White", "Black"];
+
+
+let joinedWithComma = myColor.join(",");
+console.log(joinedWithComma); 
+
+
+let joinedWithCommaAgain = myColor.join(",");
+console.log(joinedWithCommaAgain); 
+
+
+let joinedWithPlus = myColor.join("+");
+console.log(joinedWithPlus); 
