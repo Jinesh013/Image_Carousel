51b4b4dd58b3a9e1f81bd5c83575f7ab51b876e8
Jinesh Parekh <109714163+Jinesh013@users.noreply.github.com>
2024-07-08 20:54:53 +0530

    Update script1.js

--- Assignment 7/script1.js
+++ Assignment 7/script1.js
@@ -1,3 +1,13 @@
-1
-2
-3
+let myColor = ["Red", "Green", "White", "Black"];
+
+
+let joinedWithComma = myColor.join(",");
+console.log(joinedWithComma); 
+
+
+let joinedWithCommaAgain = myColor.join(",");
+console.log(joinedWithCommaAgain); 
+
+
+let joinedWithPlus = myColor.join("+");
+console.log(joinedWithPlus); 
